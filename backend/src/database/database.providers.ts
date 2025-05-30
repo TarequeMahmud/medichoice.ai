2df3cf7457fdeef8982a88a57e6c65a20e6698b7
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-05-30 07:14:32 +0600

    feat: establish postgresql database connection with typeorm.

---
+++ backend/src/database/database.providers.ts
@@ -0,0 +1,19 @@
+import { ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
