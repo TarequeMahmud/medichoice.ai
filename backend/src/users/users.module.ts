1a3e5629ce316bf73bfbe33974e7d9696bcb08c6
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-06-03 00:50:19 +0600

    fix: fix the error caused by not importing datase module on user module. fix naming inconsistency.

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { userProviders } from './users.providers';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UsersController],
   providers: [...userProviders, UsersService],
 })
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,13 +2,13 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Repository } from 'typeorm';
-import { User } from './entities/user.entity';
+import { Users } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @Inject('USER_REPOSITORY')
-    private userRepository: Repository<User>,
+    private userRepository: Repository<Users>,
   ) {}
 
   create(createUserDto: CreateUserDto) {
