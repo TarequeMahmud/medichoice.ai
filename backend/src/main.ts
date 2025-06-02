004cb1f991ba86d72a63d5ec18faf5a8f481fbac
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-06-02 23:41:02 +0600

    feat: setup swagger ui(openapi) doc for the api documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Medichoice.AI api doc')
+    .setDescription(
+      'This is the api documentation of all routes of Medichoice.AI api. Use these carefully',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
+/*
+
+*/
