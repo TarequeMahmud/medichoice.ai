ccaa0ee5e9b2ea5789c863254e5435af8214b695
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-06-22 22:27:44 +0600

    feat(MEDI-4): add passport library and jwt as strategy for the authentication system

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,29 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Users } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
-  async login(
-    loginDto: LoginDto,
-  ): Promise<{ message: string; user: Partial<Users> }> {
-    const { email, password } = loginDto;
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<Partial<Users> | null> {
     const user = await this.userService.findByEmail(email);
-    if (!user) {
-      throw new Error('User not found');
-    }
-    if (user.password !== password) {
-      throw new Error('Invalid credentials');
-    }
+    if (!user) return null;
+
+    if (user.password !== password) return null;
+
     return {
-      message: 'Login successful',
-      user: {
-        id: user.id,
-        email: user.email,
-        full_name: user.full_name,
-      },
+      id: user.id,
+      email: user.email,
+      full_name: user.full_name,
+    };
+  }
+
+  async login(loginDto: LoginDto): Promise<{ access_token: string }> {
+    console.log(loginDto);
+    const { email, password } = loginDto;
+    const user = await this.validateUser(email, password);
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    const payload = { sub: user.id, email: user.email };
+    const access_token = this.jwtService.sign(payload);
+
+    return {
+      access_token,
     };
   }
 }
