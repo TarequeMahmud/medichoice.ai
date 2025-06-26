d05166e44bbb34578cf6330c27783b67156bdb5d
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-06-26 16:06:29 +0600

    feat(MEDI-33): add medical records table table and providers to database setup

--- backend/src/records/records.module.ts
+++ backend/src/records/records.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RecordsService } from './records.service';
 import { RecordsController } from './records.controller';
+import { recordProviders } from './records.providers';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [RecordsController],
-  providers: [RecordsService],
+  providers: [...recordProviders, RecordsService],
 })
 export class RecordsModule {}
