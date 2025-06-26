c991cdee2a607b1592d6fb1ad37fe8809e9bcd2a
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-06-26 13:05:21 +0600

    feat(MEDI-24): add doctors table and providers to database setup

--- backend/src/doctors/doctors.module.ts
+++ backend/src/doctors/doctors.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DoctorsService } from './doctors.service';
 import { DoctorsController } from './doctors.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { doctorProviders } from './doctors.providers';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [DoctorsController],
-  providers: [DoctorsService],
+  providers: [...doctorProviders, DoctorsService],
+  exports: [DoctorsService],
 })
 export class DoctorsModule {}
