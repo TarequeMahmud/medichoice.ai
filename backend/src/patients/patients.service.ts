8555d601e932f21db9e352f1d20e7fb0b04c050b
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-06-27 15:21:28 +0600

    feat(MEDI-38): add data transfer, crud operations for patients module. (#24)

--- backend/src/patients/patients.service.ts
+++ backend/src/patients/patients.service.ts
@@ -1,26 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreatePatientDto } from './dto/create-patient.dto';
 import { UpdatePatientDto } from './dto/update-patient.dto';
+import { Repository } from 'typeorm';
+import { Patients } from './entities/patient.entity';
+import { UUID } from 'crypto';
 
 @Injectable()
 export class PatientsService {
-  create(createPatientDto: CreatePatientDto) {
-    return createPatientDto;
+  constructor(
+    @Inject('PATIENT_REPOSITORY')
+    private patientRepository: Repository<Patients>,
+  ) {}
+  async create(createPatientDto: CreatePatientDto): Promise<Patients> {
+    const newPatientProfile = this.patientRepository.create(createPatientDto);
+    await this.patientRepository.save(newPatientProfile);
+    return newPatientProfile;
   }
 
-  findAll() {
-    return `This action returns all patients`;
+  async findOne(id: UUID): Promise<Patients | null> {
+    return await this.patientRepository.findOne({ where: { id } });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} patient`;
+  async update(
+    id: UUID,
+    updatePatientDto: UpdatePatientDto,
+  ): Promise<Patients | null> {
+    try {
+      const patient = await this.findOne(id);
+      if (!patient) {
+        return null;
+      }
+      Object.assign(patient, updatePatientDto);
+      const updatedPatient = await this.patientRepository.save(patient);
+      return updatedPatient;
+    } catch (error: unknown) {
+      throw error;
+    }
   }
 
-  update(id: number, updatePatientDto: UpdatePatientDto) {
-    return updatePatientDto;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} patient`;
+  async remove(id: UUID): Promise<boolean> {
+    const result = await this.patientRepository.delete(id);
+    return typeof result.affected === 'number' && result.affected > 0;
   }
 }
