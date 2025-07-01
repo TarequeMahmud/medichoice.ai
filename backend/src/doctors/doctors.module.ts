f63c4703d3d52fb6ab91971c3d4bb8a67e735618
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-07-01 12:07:57 +0600

    feat(MEDI-66): add functionality for doctor to get all of his appointments (#31)

--- backend/src/doctors/doctors.module.ts
+++ backend/src/doctors/doctors.module.ts
@@ -4,9 +4,10 @@ import { DoctorsController } from './doctors.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { doctorProviders } from './doctors.providers';
 import { UsersModule } from 'src/users/users.module';
+import { AppointmentsModule } from 'src/appointments/appointments.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [DatabaseModule, UsersModule, AppointmentsModule],
   controllers: [DoctorsController],
   providers: [...doctorProviders, DoctorsService],
   exports: [DoctorsService],
