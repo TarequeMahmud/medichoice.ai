18be4053293d93e3cf80daa73014d2482068e873
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-07-12 16:51:56 +0600

    fix(MEDI-80): solve an error resulting in jwt invalid signature

--- backend/src/auth/strategy/jwt.strategy.ts
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { jwtConstants } from '../constant';
 import { JwtPayload } from 'src/common/types/auth';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtConstants.secret,
+      secretOrKey: configService.get<string>('JWT_SECRET_KEY')!,
     });
   }
 
