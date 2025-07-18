2eca68114d084bf05254a3c9b673148087523964
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-07-18 23:39:16 +0600

    feat(MEDI-97): add functionality fetch appointments from database on frontend

--- frontend/src/components/AppointmentCard.tsx
+++ frontend/src/components/AppointmentCard.tsx
@@ -1,38 +1,30 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
+import { AppointmentCardProps } from "@/types/appointment";
 
-const AppointmentCard: React.FC = () => {
+const AppointmentCard: React.FC<AppointmentCardProps> = ({ appointment }) => {
+  const appointmentsWithDetails = {
+    Doctor: appointment.doctor.full_name,
+    Time: new Date(appointment.scheduled_time).toLocaleTimeString(),
+    Symptom: appointment.reason,
+    Status: appointment.status,
+    Approved: appointment.admin_approved ? "Yes" : "No",
+    Location: appointment.clinic,
+  };
+
   return (
     <Card className="bg-[#D9D9D952] w-[60%] m-auto text-white">
       <CardTitle className="m-auto">
         Date: <em>20/20/2001</em>
       </CardTitle>
       <CardContent className="grid grid-cols-2 gap-4 m-auto">
-        <p>
-          <b>Doctor: </b>
-          <em>Dr. Alex Jonson(Dermatologist)</em>
-        </p>
-        <p>
-          <b>Time: </b>
-          <em>11.10am</em>
-        </p>{" "}
-        <p>
-          <b>Symptom: </b>
-          <em>Recurrent Boils</em>
-        </p>{" "}
-        <p>
-          <b>Status: </b>
-          <em>Completed</em>
-        </p>{" "}
-        <p>
-          <b>Approved: </b>
-          <em>Yes</em>
-        </p>{" "}
-        <p>
-          <b>Location: </b>
-          <em>Mayo Health Clinic</em>
-        </p>
+        {Object.entries(appointmentsWithDetails).map(([label, value]) => (
+          <p key={label}>
+            <b>{label}: </b>
+            <em>{value}</em>
+          </p>
+        ))}
       </CardContent>
       <CardFooter className="flex justify-around">
         <Button className="bg-green-800">Rescedule</Button>
