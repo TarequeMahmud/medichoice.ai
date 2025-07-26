de5cdfade24b13c24034e90b9bc5403a35610b57
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-07-26 22:16:36 +0600

    feat(MEDI-120): add functionality for the patient to delete the appointment

--- frontend/src/components/AppointmentCard.tsx
+++ frontend/src/components/AppointmentCard.tsx
@@ -1,12 +1,8 @@
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
-import {
-  Appointment,
-  AppointmentButtonAction,
-  AppointmentCardProps,
-} from "@/types/appointment";
-import { AppointmentActions } from "@/lib/actions";
+import { AppointmentCardProps } from "@/types/appointment";
+import { getActionsByRole } from "@/lib/actions";
 import AppointmentDetails from "./AppointmentDetails";
 
 const AppointmentCard: React.FC<AppointmentCardProps> = ({
@@ -72,46 +68,6 @@ const AppointmentCard: React.FC<AppointmentCardProps> = ({
       )}
     </>
   );
-};
-
-const getActionsByRole = (
-  role: UserRole | "",
-  appointment: Appointment,
-  setDoctor: React.Dispatch<React.SetStateAction<Doctor | null>>,
-  setViewDetails: React.Dispatch<React.SetStateAction<boolean>>
-): AppointmentButtonAction[] | null => {
-  const actions = new AppointmentActions(appointment);
-  switch (role) {
-    case "admin":
-      return [
-        {
-          label: "Approve",
-          className: "bg-green-800",
-          onClick: () => actions.approve(),
-        },
-        {
-          label: "Decline",
-          variant: "destructive",
-          onClick: () => actions.decline(),
-        },
-        { label: "View Details", onClick: () => setViewDetails(true) },
-      ];
-    case "patient":
-      return [
-        { label: "Reschedule", className: "bg-green-800" },
-        { label: "Cancel", variant: "destructive" },
-        {
-          label: "View Details",
-          onClick: async () => {
-            const [res] = await actions.doctorInfo();
-            setDoctor(res?.data);
-            setViewDetails(true);
-          },
-        },
-      ];
-    default:
-      return null;
-  }
 };
 
 export default AppointmentCard;
