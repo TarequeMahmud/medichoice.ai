66e629740f32907bfca3fe96d033b1b9d1455cdd
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-07-27 12:50:55 +0600

    feat(MEDI-106): setup basic dashboard and appointment service for doctor

---
+++ frontend/src/app/(dashboard)/doctor/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page: React.FC = () => {
+  return (
+    <div className="w-[100%]">
+      <h1>Doctor Dashboard</h1>
+      <p>Welcome to the doctor dashboard page.</p>
+    </div>
+  );
+};
+
+export default Page;
